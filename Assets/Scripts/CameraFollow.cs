edebafdb24763792d71c687318578f6a335aace5
Knok <kareemahmed1742005@gmail.com>
2025-03-25 02:58:56 +0200

    Cam

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject player;
+    public Vector3 Offset;
+    public float Damping;
+    Vector3 val = Vector3.zero;
+    void FixedUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position,player.transform.position + Offset, ref val, Damping);
+    }
+}

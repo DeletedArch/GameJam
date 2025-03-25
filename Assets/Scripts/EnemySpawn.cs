89229c665739db081c6462a3580d611b81987121
Knok <kareemahmed1742005@gmail.com>
2025-03-26 00:59:40 +0200

    EnemySpawn

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public Transform[] SpawnPoint;
+    public GameObject Enemies;
+    int randSpawn;
+    public float SpawnRate = 1.0f;
+    private void Start()
+    {
+        StartCoroutine(Spawner());
+    }
+    
+    
+    private IEnumerator Spawner()
+    {
+        WaitForSeconds wait= new WaitForSeconds(SpawnRate);
+        while (true)
+        {
+            yield return wait;
+            randSpawn = Random.Range(0, SpawnPoint.Length);
+            Instantiate(Enemies, SpawnPoint[randSpawn].position, transform.rotation);
+        }
+    }
+}

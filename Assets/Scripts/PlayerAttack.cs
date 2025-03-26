335ceb56c6958869921c7cd041ccbea88924f82c
Ahmed Elwakil <ahmedbasha243@gmail.com>
2025-03-26 02:35:36 +0200

    attack animation

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public SpriteRendererScript spriteRendererScript;
+
+    public void Awake()
     {
-        
+        spriteRendererScript = gameObject.GetComponent<SpriteRendererScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool Shoot = Input.GetButtonDown("Fire1");
+        if (Shoot)
+        {
+            spriteRendererScript.isAttacking = true;
+            CallAfterDelay.Create(0.2f, () => spriteRendererScript.isAttacking = false);
+        }
     }
 }

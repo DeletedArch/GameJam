701624a982d63d2dd156ef3115c169c7f6b1a2b4
AhmedShara3 <sharaaahmed16@gmail.com>
2025-03-28 22:57:10 +0200

    Main menu

---
+++ Assets/Scripts/MainMenue.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenue : MonoBehaviour
+{
+  public void PlayButton()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+
+  }
+  public void QuitGame()
+  {
+    Application.Quit();
+  }
+}

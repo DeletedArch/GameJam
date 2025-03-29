5057dd0b71e24c01298b1034b78727a9e341e0a0
AhmedShara3 <sharaaahmed16@gmail.com>
2025-03-29 17:19:02 +0200

    Score UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Text scoreText;
+    int score = 0 ;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText.text = score.ToString() + " Coins";
     }
 
     // Update is called once per frame

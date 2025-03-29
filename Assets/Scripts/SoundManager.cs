c602cc634cca7eff2efd125541d15c561642b9d9
AhmedShara3 <sharaaahmed16@gmail.com>
2025-03-30 01:18:20 +0200

    soundmanager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum SoundType
+{
+    Sowrd,
+    fire,
+    punch,
+    FootSteps
+
+}
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{   [SerializeField] private AudioClip[] soundList; 
+    private static SoundManager instance;
+    private AudioSource audioSource;
+    void Awake()
+    {
+        instance = this;
+        
+    }
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public static void PlaySound(SoundType sound , float volume = 1f)
+    {
+        instance.audioSource.PlayOneShot(instance.soundList[(int)sound],volume);
+    }
+}

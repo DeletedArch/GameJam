dbbf19238ee39127b7a391aed77c1410a997864a
AhmedShara3 <sharaaahmed16@gmail.com>
2025-03-30 20:25:43 +0200

    level loader

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public GameManager gameManager;
+    public float transitionTime = 1f;
+    void Start()
+    {
+        gameManager = GetComponent<GameManager>();
+    }
+    void Update()
+    {
+       // if(gameManager.score == 1)
+        {
+            LoadNextLevel();
+        }
+    }
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public Text scoreText;
-    int score = 0 ;
+    public int score = 0 ;
     // Start is called before the first frame update
     
     public GameObject GameOverPanel;

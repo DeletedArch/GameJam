3a4b123cdf084cc9ca1c5208d58169eb3d3faaf3
AhmedShara3 <sharaaahmed16@gmail.com>
2025-03-30 03:34:12 +0200

    the boss scene

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public float maxHealth = 100 ;
+    public int currentHealth;
+    public float AttackCoolDawn = 1.0f;
+    public SpriteRendererScript spriteRenderer;
+    public GameObject Coin;
+    public HealthBar healthBar;
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRendererScript>();
+    }
+    private IEnumerator EnemyAttackAnimation()
+    {
+        WaitForSeconds wait = new WaitForSeconds(AttackCoolDawn);
+        while (true)
+        {
+            yield return wait;
+            spriteRenderer.isAttacking = true;
+            SoundManager.PlaySound(SoundType.Sowrd);
+            // spriteRenderer.isRunning = false;
+            CallAfterDelay.Create(0.5f, () => spriteRenderer.isAttacking = false);
+            // CallAfterDelay.Create(1f, () => spriteRenderer.isRunning = true);
+
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Hit");
+        if (other.gameObject.tag == "Damage")
+        {
+            maxHealth -= other.gameObject.GetComponent<DamageScript>().Damage;
+            healthBar.SetHealth(currentHealth);
+            if (maxHealth <= 0)
+            {
+                Instantiate(Coin, transform.position, transform.rotation);
+                Destroy(gameObject);
+            }
+        }
+        else if(other.gameObject.tag == "Player")
+        {
+            SoundManager.PlaySound(SoundType.Sowrd);
+            spriteRenderer.isAttacking = true;
+            // spriteRenderer.isRunning = false;
+            CallAfterDelay.Create(0.5f, () => spriteRenderer.isAttacking = false);
+            // CallAfterDelay.Create(1f, () => spriteRenderer.isRunning = true);
+            Debug.Log("Hit a player");
+            StartCoroutine(EnemyAttackAnimation());
+            
+
+        }
+    }
+    
+     private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Exit");
+        StopAllCoroutines();
+        
+    }
+
+}
+

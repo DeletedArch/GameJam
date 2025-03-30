917934216a70a7228c82341a784b5a1c7517c716
Ahmed Elwakil <ahmedbasha243@gmail.com>
2025-03-31 01:45:20 +0200

    boss throws shurikens

---
+++ Assets/Scripts/DamageEnemyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageEnemyScript : MonoBehaviour
+{
+    public Rigidbody2D FireBall;
+    public float Damage = 10f;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            FireBall.velocity = Vector3.zero;
+            OnDestroy();
+        }
+    }
+    public void OnDestroy()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Shuriken.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System;
+using System.Threading.Tasks;
+
+public class Shuriken : MonoBehaviour
+{
+    public GameObject Bullet;
+    public Transform SpawnPoint;
+    public float FireForce = 100f;
+    public Transform Target;
+    public float DamageCooldownTime = 1f;
+    public bool DamageCooldown = false;
+    Vector3 ve = new Vector3(0f, -0.6f,0f);
+    float Distance2;
+
+
+    public void FixedUpdate()
+    {
+        Distance2 = Vector2.Distance(transform.position, Target.transform.position);
+        if (DamageCooldown == false && Distance2 >= 5f)
+        {
+            DamageCooldown = true;
+            Debug.Log("Fire");
+            SoundManager.PlaySound(SoundType.fire);
+            GameObject Temp = Instantiate(Bullet, SpawnPoint.position + ve , SpawnPoint.rotation);
+            
+            if (Temp != null) {
+                
+                Vector2 firePos = -SpawnPoint.position + Target.position;
+                Temp.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(firePos.y, firePos.x) * Mathf.Rad2Deg);
+                Temp.GetComponent<Rigidbody2D>().AddForce(firePos.normalized * FireForce * Time.deltaTime);
+            }
+            
+            CallAfterDelay.Create(5, () => DestroyGameObject(ref Temp));
+            CallAfterDelay.Create(DamageCooldownTime, () => DamageCooldown = false);
+        }
+
+    }
+
+    private void DestroyGameObject(ref GameObject obj)
+    {
+        Debug.Log("Destroying GameObject");
+        Destroy(obj);
+    }
+}

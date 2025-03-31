2353c34a54ac668b5ba04013857860828fee7c6a
Ahmed Elwakil <ahmedbasha243@gmail.com>
2025-03-31 02:34:34 +0200

    fix build

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.SceneManagement;
-using UnityEditor.Build.Player;
 
 public class LevelLoader : MonoBehaviour
 {
